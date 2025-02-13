dfabc53f088122facb20ac5d4b1b013195081b2a
wYeha <snovavzloman@mail.ru>
2025-02-13 16:07:15 +0300

    slider done

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
-import TabsCollection from "./Tabs.js";
+import TabsCollection from "./Tabs.js"
+import Slider from "./Slider.js"
 
 new TabsCollection()
+
+
+let slider = new Slider('.slider', '.slider__list', '.product-card__link')
